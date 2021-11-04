3d2b34c37d91783cc3cbbdf3fc347bc891734aa1
Warrys Akadiri <wakadiri99@gmail.com>
2021-11-04 15:06:29 -0500

    MyFirstCommit

---
+++ JavaTutorial/src/main/java/LearnJava.java
@@ -0,0 +1,12 @@
+
+public class LearnJava {
+	public static void main (String[]args) {
+		String name = "Susan";
+		String wall = "Derrick";
+		System.out.println(name.toLowerCase());
+		System.out.println(wall.toUpperCase());
+		
+	}
+	
+
+}
